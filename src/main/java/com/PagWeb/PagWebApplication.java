1ac197eef1b012a6b80f70c69c80c86d84497ce0
scv13 <scv13@192.168.0.102>
2022-02-17 23:58:04 -0600

    Desarrollo de la tarea#2

---
+++ src/main/java/com/PagWeb/PagWebApplication.java
@@ -0,0 +1,13 @@
+package com.PagWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PagWeb/PagWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PagWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
